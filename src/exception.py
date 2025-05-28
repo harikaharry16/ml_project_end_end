4b6ffc88a80c954b3fc32f7bb63fa3bdd2c1f7ab
harikasunnapu <harikasunnapu16@gmail.com>
2025-05-28 12:11:06 +0530

    2nd:setup,requirements,src

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def error_message_detail(error,error_detail:sys):
+    # getting where error is getting 
+    _,_,exc_tb = error_detail.exc_info()
+    # how to get file name from exc_tb
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    # to format error message
+    error_message = "Error occured in file name [{0}] in line number [{1}] error is [{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error)
+
+    return error_message
+
+    )
+
+
+class customException(exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
